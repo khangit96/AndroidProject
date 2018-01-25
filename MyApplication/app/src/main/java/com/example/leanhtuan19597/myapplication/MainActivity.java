da424c59f08589bd720107d92bf2fe92ea71d4db
leanhtuan19597 <leanhtuan19597@gmail.com>
2018-01-25 20:09:05 +0700

    mjfkfkf

--- MyApplication/app/src/main/java/com/example/leanhtuan19597/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/leanhtuan19597/myapplication/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.leanhtuan19597.myapplication;
 import android.app.Application;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -26,6 +31,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import ai.api.AIDataService;
+import ai.api.AIServiceException;
+import ai.api.android.AIConfiguration;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
 
 public class MainActivity extends AppCompatActivity {
     SwitchCompat switchRelay1, switchRelay2, switchRelay3, switchRelay4, switchRelay5, switchRelay6;
@@ -230,5 +245,102 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mnVoice) {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, 10);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void queryFromAPIAI(String query) {
+        /*api ai*/
+        final AIConfiguration config = new AIConfiguration("5da52399d5b14bcda14f3f091e0f5f13",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        final AIDataService aiDataService = new AIDataService(config);
+
+        final AIRequest aiRequest = new AIRequest();
+        aiRequest.setQuery(query);
+
+        new AsyncTask<AIRequest, Void, AIResponse>() {
+            @Override
+            protected AIResponse doInBackground(AIRequest... requests) {
+                final AIRequest request = requests[0];
+                try {
+                    final AIResponse response = aiDataService.request(aiRequest);
+                    return response;
+                } catch (AIServiceException e) {
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(AIResponse aiResponse) {
+                if (aiResponse != null) {
+                    final Result result = aiResponse.getResult();
+                    String action = result.getAction().toString();
+
+                    if (action.equals("BatDen1")) {
+                        switchRelay1.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay1/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen1")) {
+                        switchRelay1.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay1/TurnOn").setValue(false);
+                    } else if (action.equals("BatDen2")) {
+                        switchRelay2.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay2/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen2")) {
+                        switchRelay2.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay2/TurnOn").setValue(false);
+                    } else if (action.equals("BatDen3")) {
+                        switchRelay3.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay3/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen3")) {
+                        switchRelay3.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay3/TurnOn").setValue(false);
+                    } else if (action.equals("BatDen4")) {
+                        switchRelay4.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay4/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen4")) {
+                        switchRelay4.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay4/TurnOn").setValue(false);
+                    } else if (action.equals("BatDen5")) {
+                        switchRelay5.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay5/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen5")) {
+                        switchRelay5.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay5/TurnOn").setValue(false);
+                    } else if (action.equals("BatDen6")) {
+                        switchRelay6.setChecked(true);
+                        mDatabase.child("MinhTrung/Relay6/TurnOn").setValue(true);
+                    } else if (action.equals("TatDen6")) {
+                        switchRelay6.setChecked(false);
+                        mDatabase.child("MinhTrung/Relay6/TurnOn").setValue(false);
+                    }
+                }
+            }
+        }.execute(aiRequest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String textRespone = result.get(0).toString();
+                queryFromAPIAI(textRespone);
+
+            }
+        }
     }
 }
